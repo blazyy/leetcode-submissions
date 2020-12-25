9839e3e4ee27eee8334dd6d76f09ac07a393f2c0
blazyy <razeenfaaez@gmail.com>
2020-12-25 18:20:31 +0530

    Time: 108 ms (8.76%), Space: 21.2 MB (22.88%) - LeetHub

---
+++ contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        if(nums.size() <= 1) return false;
+        set <int> num_set;
+        for(auto num : nums){
+            if(num_set.find(num) != num_set.end())
+                return true;
+            num_set.insert(num);
+        }
+        return false;
+    }
+};
