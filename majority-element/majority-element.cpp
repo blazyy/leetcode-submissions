92f9709daa8ab150533957a1bb7f57e16bd4991e
blazyy <razeenfaaez@gmail.com>
2020-12-26 12:37:35 +0530

    Time: 32 ms (95.72%), Space: 20 MB (40.99%)

--- majority-element/majority-element.cpp
+++ majority-element/majority-element.cpp
@@ -1,18 +1,45 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-        int to_return;
-        unordered_map <int, int> counts;
-        for(auto num : nums){
-            if(counts.find(num) != counts.end()) counts[num]++;
-            else counts[num] = 1;
-        }
-        for(auto count : counts){
-            if(count.second > nums.size() / 2){
-                to_return = count.first;
-                break;                
-            }
-        }
-        return to_return;
-    }
+    int majorityElement(vector<int>& nums) {
+        // Moore's Majority Vote Algorithm
+        int candidate = nums[0], count = 1;
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] == candidate) count++;
+            else{
+                count--;
+                if(count == 0){
+                    candidate = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        return candidate;
+    }
 };
+
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         return nums[nums.size() / 2];
+//     }
+// };
+
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int to_return;
+//         unordered_map <int, int> counts;
+//         for(auto num : nums){
+//             if(counts.find(num) != counts.end()) counts[num]++;
+//             else counts[num] = 1;
+//         }
+//         for(auto count : counts){
+//             if(count.second > nums.size() / 2){
+//                 to_return = count.first;
+//                 break;                
+//             }
+//         }
+//         return to_return;
+//     }
+// };
