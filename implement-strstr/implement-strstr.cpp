3e1e108e6ac7e9aedf0e44ef5374cc3254302f40
blazyy <razeenfaaez@gmail.com>
2020-12-27 00:24:09 +0530

    Time: 4 ms (65.11%), Space: 7.5 MB (17.24%) - LeetHub

--- implement-strstr/implement-strstr.cpp
+++ implement-strstr/implement-strstr.cpp
@@ -36,16 +36,28 @@ public:
         return lps;
     }
 };
-​
+
 // class Solution {
 // public:
-//     int strStr(string haystack, string needle) {
-//         if(needle == "")
-//             return 0;
-//         if(haystack == "" || haystack.length() < needle.length())
-//             return -1;
-//         int nptr = 0;
-//         for(int i = 0; i < haystack.length(); i++){
-//             if(haystack[i] == needle[nptr]){
-//                 if(haystack.length() - i < needle.length())
-//                     return -1;
+//     int strStr(string haystack, string needle) {
+//         if(needle == "")
+//             return 0;
+//         if(haystack == "" || haystack.length() < needle.length())
+//             return -1;
+//         int nptr = 0;
+//         for(int i = 0; i < haystack.length(); i++){
+//             if(haystack[i] == needle[nptr]){
+//                 if(haystack.length() - i < needle.length())
+//                     return -1;
+//                 for(int j = i; j < i + needle.length(); j++){
+//                     if(haystack[j] != needle[nptr++]){
+//                         nptr = 0;
+//                         break;
+//                     }
+//                 }
+//                 if(nptr == needle.length()) return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }; 
