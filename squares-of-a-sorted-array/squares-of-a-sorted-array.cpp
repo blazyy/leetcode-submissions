b4b442eddf1346687c25b79f9d32302f3f16d4dc
blazyy <razeenfaaez@gmail.com>
2020-12-28 17:31:06 +0530

    Time: 44 ms (96.25%), Space: 27.4 MB (10.51%) - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -30,3 +30,14 @@ public:
         return squares;
     }
 };
+
+// Old - O(nlogn)
+// class Solution {
+// public:
+//     vector<int> sortedSquares(vector<int>& nums) {
+//         for(int i = 0; i < nums.size(); i++)
+//             nums[i] = pow(nums[i], 2);
+//         sort(nums.begin(), nums.end());
+//         return nums;
+//     }
+// };
