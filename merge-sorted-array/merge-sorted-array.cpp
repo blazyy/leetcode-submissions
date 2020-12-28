36ce0bec74c6df53d7bcf5c1e79a688f22afdc38
blazyy <razeenfaaez@gmail.com>
2020-12-28 16:30:19 +0530

    Time: 0 ms (100.38%), Space: 9.2 MB (97.01%) - LeetHub

--- merge-sorted-array/merge-sorted-array.cpp
+++ merge-sorted-array/merge-sorted-array.cpp
@@ -1,8 +1,29 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i = m, j = 0; i < m + n; i++, j++)
-            nums1[i] = nums2[j];
-        sort(nums1.begin(), nums1.end());
-    }
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) { 
+        m--, n--;
+        int index = nums1.size() - 1;
+        while(index >= 0) {
+            if(m < 0)
+                nums1[index] = nums2[n--];
+            else if(n < 0)
+                nums1[index] = nums1[m--];
+            else
+                if(nums1[m] < nums2[n])
+                    nums1[index] = nums2[n--];
+                else
+                    nums1[index] = nums1[m--];
+            index--;
+        }
+    }
 };
+
+// Old Solution. Bad. O((n + m)log(n + m)).
+// class Solution {
+// public:
+//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+//         for(int i = m, j = 0; i < m + n; i++, j++)
+//             nums1[i] = nums2[j];
+//         sort(nums1.begin(), nums1.end());
+//     }
+// };
