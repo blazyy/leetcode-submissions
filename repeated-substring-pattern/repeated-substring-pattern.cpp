5141a0343645b78876989c18903ea9664c261861
blazyy <razeenfaaez@gmail.com>
2020-12-31 20:42:32 +0530

    Time: 20 ms (77.63%), Space: 13.6 MB (58.04%) - LeetHub

--- repeated-substring-pattern/repeated-substring-pattern.cpp
+++ repeated-substring-pattern/repeated-substring-pattern.cpp
@@ -1,10 +1,45 @@
-// Concatenate string onto itself. Remove first and last char. If original string exists in new string, the string is a repeated substring pattern.
+// Using KMP, compute LPS array.
+// Value at last element of LPS array gives length of substring.
+// Check if the substring length is divisible by the length of the entire string.
+// If so, string is a repeated substring pattern.
 // Time Complexity  - O(n)
-// Space Complexity - O(1)
+// Space Complexity - O(n)
 ​
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        return (s + s).substr(1, 2 * s.size() - 2).find(s) != string::npos;
+        vector <int> lps = computeLPS(s);
+        int n = lps.size() - 1;
+        return lps[n] and lps[n] % (n + 1 - lps[n]) == 0; 
+    }
+    vector <int> computeLPS(string str) {
+        vector <int> lps (str.length(), 0);
+        lps[0] = 0;
+        int i = 1, j = 0;
+        while(i < str.length()) {
+            if(str[i] == str[j]) 
+                lps[i++] = ++j;
+            else {
+                if(j != 0) j = lps[j - 1];
+                else lps[i++] = 0;
+            }
+        }
+        return lps;
     }
 };
+​
+// Alternate Solution
+​
+// Concatenate string onto itself. Remove first and last char. If original string exists in new string, the string is a repeated substring pattern.
+​
+// Time Complexity  - O(n)
+// Space Complexity - O(1)
+​
+// class Solution {
+// public:
+//     bool repeatedSubstringPattern(string s) {
+//         return (s + s).substr(1, 2 * s.size() - 2).find(s) != string::npos;
+//     }
+// };
+​
+​
