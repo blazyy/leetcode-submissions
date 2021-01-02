c79384fae16d87761e348914addeae49e7d9e2a7
blazyy <razeenfaaez@gmail.com>
2021-01-02 11:24:47 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (89.53%) - LeetHub

--- multiply-strings/multiply-strings.cpp
+++ multiply-strings/multiply-strings.cpp
@@ -13,19 +13,19 @@ public:
         
         for(int i = m - 1; i >= 0; i--) {
             p = m + n - 1 - num_zeros;
-            for(int j = n - 1; j >=0; j--) {
-                curr_sum = (num1[i] - '0') * (num2[j] - '0') + (product[p] - '0') + carry;
+            for(int j = n - 1; j >= 0; j--) {
+                curr_sum = (num1[i] - '0') * (num2[j] - '0') + (product[p] - '0') + carry; // Instead of going level by level like in manual multiplication, do all the calculations on one line
                 product[p--] = (curr_sum % 10) + '0';
                 carry = curr_sum / 10;                
                 if((i == 0 or j == 0) and carry) {
-                    product[p] = ((product[p] - '0') + carry) + '0'; // At last digit, where there's nowhere to add the carry to.
-                    carry = 0;
+                    product[p] = ((product[p] - '0') + carry) + '0'; // When there's nowhere to add the carry to, just put the carry one digit back
+                    carry = 0; // Reset carry
                 }
             }
-            num_zeros++;
+            num_zeros++; // Used to emulate zeroes added in manual multiplication. Here it just moves p forward (or backward in terms of indices)
         }
         if(product[p] != '0')
-            return product.substr(p, m + n);
-        return product.substr(p + 1, m + n);
+            return product.substr(p, m + n); // For cases where the size of the new number is m + n 
+        return product.substr(p + 1, m + n); // For cases where the size of the new number m + n - 1
     }
 };
