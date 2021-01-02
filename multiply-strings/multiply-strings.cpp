6d723e0c16625839b075259a1100e44446d0072d
blazyy <razeenfaaez@gmail.com>
2021-01-02 11:32:36 +0530

    Time: 4 ms (89.43%), Space: 6.7 MB (89.53%) - LeetHub

--- multiply-strings/multiply-strings.cpp
+++ multiply-strings/multiply-strings.cpp
@@ -6,8 +6,7 @@ class Solution {
 public:
     string multiply(string num1, string num2) {
         
-        if(num1 == "0" or num2 == "0")
-            return "0";
+        if(num1 == "0" or num2 == "0") return "0";
         
         int m = num1.length();
         int n = num2.length();
