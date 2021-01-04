a920379e7b51f8540874718985b78c9f0e6f494e
blazyy <razeenfaaez@gmail.com>
2021-01-04 13:17:59 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (80.87%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,21 +1,25 @@
+// Recursive DFS. Pretty self explanatory
+// Time Complexity  - O(4 ^ n) where n is the number of digits, and 4 is the maximum possible number of branches
+// Space Complexity - O(n) where n is the number of digits i.e. depth of the recurisve tree
+​
 class Solution {
 public:
     vector <string> letterCombinations(string digits) {
         vector <string> result;
-        if(digits.size() == 0)
+        if(digits.length() == 0)
             return result;
         vector <string> mappings = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         letterCombinationsRecursive(result, mappings, digits, "", 0);
         return result;
-    }
+    }
     
     void letterCombinationsRecursive(vector <string> &result, vector <string> &mappings, string digits, string current, int index) {
-        if(index == digits.size()) { // When all digits have been processed, add current string to result, and return.
+        if(index == digits.length()) {
             result.push_back(current);
-            return; 
+            return;
         }
         string letters = mappings[digits[index] - '0' - 2];
-        for(auto letter : letters) // Recursively call function on each letter, increasing index by 1 and adding the current letter to current
+        for(auto letter : letters)
             letterCombinationsRecursive(result, mappings, digits, current + letter, index + 1);
     }
 };
