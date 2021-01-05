a283aa62c21c0858556de51eaee711516e0ca534
blazyy <razeenfaaez@gmail.com>
2021-01-05 14:46:19 +0530

    Time: 12 ms (59.21%), Space: 19.5 MB (35.50%) - LeetHub

--- maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -10,31 +10,42 @@
  * };
  */
 ​
+// Recursive DFS.
+// TC - O(n) ?
+// SC - O(n) ?
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr) return 0;
+        return max(maxDepth(root -> left), maxDepth(root -> right)) + 1;
+    }
+};
+​
 // BFS. Depth increases when all nodes of a level have been dealt with.
 // TC - O(n)
 // SC - O(n)
-class Solution {
-public:
-    int maxDepth(TreeNode *root) {
-        if(root == nullptr) return 0;
-        int max_depth = 0, depth = 0, nodes_in_current_level;
-        queue <TreeNode *> q;
-        TreeNode *current = root;
-        q.push(current);
-        while(q.size()) {
-            nodes_in_current_level = q.size();
-            while(nodes_in_current_level) {
-                current = q.front(); q.pop();
-                if(current -> left) q.push(current -> left);
-                if(current -> right) q.push(current -> right);
-                nodes_in_current_level--;
-            }
-            depth++;
-            max_depth = max(max_depth, depth);
-        }
-        return max_depth;
-    }
-};
+// class Solution {
+// public:
+//     int maxDepth(TreeNode *root) {
+//         if(root == nullptr) return 0;
+//         int max_depth = 0, depth = 0, nodes_in_current_level;
+//         queue <TreeNode *> q;
+//         TreeNode *current = root;
+//         q.push(current);
+//         while(q.size()) {
+//             nodes_in_current_level = q.size();
+//             while(nodes_in_current_level) {
+//                 current = q.front(); q.pop();
+//                 if(current -> left) q.push(current -> left);
+//                 if(current -> right) q.push(current -> right);
+//                 nodes_in_current_level--;
+//             }
+//             depth++;
+//             max_depth = max(max_depth, depth);
+//         }
+//         return max_depth;
+//     }
+// };
 ​
 // Iterative DFS. We keep track of current depth using a stack. Each item in the depths stack corresponds to the depth of the item in the same place/index as the node in the other stack.
 // TC - O(n) where n is the number of nodes in the tree
@@ -51,3 +62,15 @@ public:
 //             current = s.top(); cout << current -> val << " "; s.pop();
 //             int temp = depths.top(); depths.pop();
 //             max_depth = max(max_depth, temp);
+//             if(current -> right) {
+//                 s.push(current -> right);
+//                 depths.push(temp + 1);
+//             }
+//             if(current -> left) {
+//                 s.push(current -> left);
+//                 depths.push(temp + 1);
+//             }
+//         }
+//         return max_depth;
+//     }
+// };
