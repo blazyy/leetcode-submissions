56fd039c73f54c845b14d903ae79731da8a0016a
blazyy <razeenfaaez@gmail.com>
2021-01-05 22:21:30 +0530

    Time: 4 ms (46.83%), Space: 8.9 MB (54.05%) - LeetHub

--- binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -9,19 +9,43 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// Iterative Soln.
+// TC - O(n)
+// SC - O(h)
 class Solution {
 public:
-    vector <int> traversal;
-    
     vector <int> inorderTraversal(TreeNode* root) {
-        inorder(root);
+        vector <int> traversal;
+        stack <TreeNode *> s;
+        TreeNode *current = root;
+        while(true) {
+            if(current) {
+                s.push(current);
+                current = current -> left;
+            }
+            else if (s.size()) {
+                current = s.top(); s.pop();
+                traversal.push_back(current -> val);
+                current = current -> right;
+            }
+            else break;
+        }
         return traversal;
     }
-    
-    void inorder(TreeNode *root) {
-        if(!root) return;
-        inorder(root -> left);
-        traversal.push_back(root -> val);
-        inorder(root -> right);
-    }
 };
+​
+// Recursive solution
+// TC - O(n)
+// SC - O(h)
+// class Solution {
+// public:
+//     vector <int> traversal;
+    
+//     vector <int> inorderTraversal(TreeNode* root) {
+//         inorder(root);
+//         return traversal;
+//     }
+    
+//     void inorder(TreeNode *root) {
+//         if(!root) return;
+//         inorder(root -> left);
