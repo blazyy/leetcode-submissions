f388cc5a61bf967b9a78f94e79d026d73d35ea7b
blazyy <razeenfaaez@gmail.com>
2021-01-10 20:14:57 +0530

    Time: 4 ms (47.09%), Space: 8.9 MB (54.34%) - LeetHub

--- binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -49,7 +49,3 @@ public:
 //     void inorder(TreeNode *root) {
 //         if(!root) return;
 //         inorder(root -> left);
-//         traversal.push_back(root -> val);
-//         inorder(root -> right);
-//     }
-// };
